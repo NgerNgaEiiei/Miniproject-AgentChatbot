aa14cbf20497b1a0fba71a59c73909256a3f09e6
Kanokporn Phanpunya - 6609650152 <145310309+NgerNgaEiiei@users.noreply.github.com>
2026-03-14 15:45:47 +0700

    ดูข้อมูลใน vector db ด้วย qdrant dashboard และเปลี่ยนไลบรารี pdfplumber -> pymupdf

--- rag/ingest.py
+++ rag/ingest.py
@@ -1,28 +1,31 @@
-import pdfplumber # อ่านไฟล์ PDF ดึงข้อความออกมา
+#import pdfplumber # อ่านไฟล์ PDF ดึงข้อความออกมา
+import fitz 
+import re
+from docx import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter # แบ่งข้อความยาว → เป็น chunk เล็ก ๆ
 from sentence_transformers import SentenceTransformer # แปลง text → vector (embedding)
 from qdrant_client import QdrantClient # เชื่อมต่อกับ Qdrant Vector Database
 from qdrant_client.models import VectorParams, Distance, PointStruct # ใช้กำหนดโครงสร้างของ vector database
 
+
 def load_pdf(path):
     text = ""
+    doc = fitz.open(path)
+    for page in doc:
+        extracted = page.get_text("text")
+        if extracted:
+            cleaned = re.sub(r'\n', ' ', extracted)
+            cleaned = re.sub(r'\s+', ' ', cleaned)
+            text += cleaned + "\n"
 
-    with pdfplumber.open(path) as pdf:
-        for page in pdf.pages:
-            text += page.extract_text() + "\n"
-
-    return text
+    return text 
 
 
-def split_text(text):
+def split_text(text): # หาเพิ่ม
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500, # แต่ละ chunk ~500 ตัวอักษร
-        chunk_overlap=100 # chunk จะทับกัน 100 ตัวอักษร
-        # Chunk 1
-            # [ 0 ----------- 500 ]
-
-        # Chunk 2
-                    # [400 ----------- 900]
+        chunk_size=600,     # แต่ละ chunk 500 ตัวอักษร
+        chunk_overlap=50,   # chunk จะทับกัน 100 ตัวอักษร
+        separators=["\n\d+\.", "\n", " ", ""],
     )
 
     chunks = splitter.split_text(text)
@@ -30,7 +33,7 @@ def split_text(text):
     return chunks
 
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+model = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2") # Model สำหรับทำ embedding
 
 def create_embeddings(chunks):
     embeddings = model.encode(chunks)
@@ -38,16 +41,16 @@ def create_embeddings(chunks):
     return embeddings
 
 
-client = QdrantClient(path="qdrant_db")         # เชื่อมต่อ Qdrant
+client = QdrantClient("localhost", port=6333)       # เชื่อมต่อ Qdrant
 
 # ลบ collection เก่า ป้องกันข้อมูลซ้ำ
 try:
-    client.delete_collection("curriculum")
+    client.delete_collection("coursesdetail")
 except:
     pass
 
 client.create_collection(               # สร้าง collection ใหม่ใน vector database
-    collection_name="curriculum",       # ตั้งชื่อ
+    collection_name="coursesdetail",       # ตั้งชื่อ
     vectors_config=VectorParams(        
         size=384,                       # vector dimension ต้องตรงกับ embedding model
         distance=Distance.COSINE        # ใช้ Cosine similarity สำหรับวัดว่า vector ไหนคล้ายกัน
@@ -59,24 +62,30 @@ def store_vectors(chunks, embeddings):
     points = []     # สร้าง list ไว้เก็บข้อมูล
 
     for i, (chunk, emb) in enumerate(zip(chunks, embeddings)):
+        # ลองดึงรหัสวิชา เช่น คพ.xxx หรือ CSxxx ออกมาเป็น metadata
+        # course_code = re.findall(r'[คพCS]+\.?\s?\d+', chunk)
+        
         points.append(                      # เพิ่มข้อมูลลง list
             PointStruct(
                 id=i,                       # กำหนด id 0, 1, 2, 3,...
                 vector=emb,                 # vector ของ chunk นั้น
-                payload={"text": chunk}     # เก็บ text จริงไว้ด้วย เวลาค้นหา vector จะได้ vector → text
+                payload={
+                    "text": chunk,
+                    # "course_codes": course_code
+                }     
             )
         )
 
     client.upsert(                          # update + insert ข้อมูลลง database
-        collection_name="curriculum",
+        collection_name="coursesdetail",
         points=points                       # เอา list vectors ที่เราสร้างมาเก็บ
     )
 
 
 if __name__ == "__main__":
 
-    text = load_pdf("data/curriculum.pdf")  # เรียก load_pdf() แปลงไฟล์ pdf เป็น text
-
+    text = load_pdf("data/coursesdetail.pdf")  # เรียก load_pdf() แปลงไฟล์ pdf เป็น text
+    
     chunks = split_text(text)               # แบ่ง text เป็น chunk
 
     embeddings = create_embeddings(chunks)  # แปลง chunk text → vectors
